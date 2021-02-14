456bbac3bad150d5c9bcbfee8c2b3ecb9f721677
volth <volth@volth.com>
2021-02-14 14:07:35 +0000

    [cpan2nix] perlPackages.MusicBrainzDiscID: cleanup

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -14712,6 +14712,7 @@ let
     doCheck = false; # The main test performs network access
     nativeBuildInputs = [ pkgs.pkg-config ];
     propagatedBuildInputs = [ pkgs.libdiscid ];
+    buildInputs = [ TestPod ];
   };
 
   MusicBrainz = buildPerlModule {
