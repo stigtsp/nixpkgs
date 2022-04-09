76643994447ac489fdc6d1896096ea5e5494d883
Stig Palmquist <stig@stig.io>
2022-04-10 00:04:39 +0200

    perlPackages.Mojolicious: 9.19 -> 9.23

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -14515,10 +14515,10 @@ let
 
   Mojolicious = buildPerlPackage {
     pname = "Mojolicious";
-    version = "9.19";
+    version = "9.23";
     src = fetchurl {
-      url = "mirror://cpan/authors/id/S/SR/SRI/Mojolicious-9.19.tar.gz";
-      sha256 = "15qs99sl3ckzqwpqk4kawhamdm6160bzxyikf3blym4fn1k6s1a5";
+      url = "mirror://cpan/authors/id/S/SR/SRI/Mojolicious-9.23.tar.gz";
+      sha256 = "sha256-8/YYxwDhgUTnxSnF5hzSo3xA/QJceTWOWeXpw/4BKsY=";
     };
     meta = {
       homepage = "https://mojolicious.org";
