1b73f56564b5971bd43c600ded9713f850ca4167
Stig Palmquist <stig@stig.io>
2022-04-10 00:31:08 +0200

    perlPackages.PodStrip: 1.02 -> 1.100

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -18913,10 +18913,10 @@ let
 
   PodStrip = buildPerlModule {
      pname = "Pod-Strip";
-     version = "1.02";
+     version = "1.100";
      src = fetchurl {
-       url = "mirror://cpan/authors/id/D/DO/DOMM/Pod-Strip-1.02.tar.gz";
-       sha256 = "1zsjfw2cjq1bd3ppl67fdvrx46vj9lina0c3cv9qgk5clzvaq3fq";
+       url = "mirror://cpan/authors/id/D/DO/DOMM/Pod-Strip-1.100.tar.gz";
+       sha256 = "sha256-Z1BqZh+pyuzv57pPQvC8FbCm8JZ8eWB3QPbLaXSu1M0=";
      };
      meta = {
        description = "Remove POD from Perl code";
